b54be1fd82b8ba564750909e063e19e69ade4cc6
robhyrkiel <rob.hyrkiel@gmail.com>
2019-09-04 14:48:38 -0400

    chore: add files for first commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+//Adds script and stylesheets
+function inhabitant_files() {
+    wp_enqueue_style('inhabitant_styles', get_stylesheet_uri('/build/css/style.min.css'), NULL, microtime());
+    wp_enqueue_style('fonts', "https://fonts.googleapis.com/css?family=Lato&display=swap");
+}
+
+add_action('wp_enqueue_scripts', 'inhabitant_files');
+
+//Adds theme support - ex: title tag
+function inhabitant_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'inhabitant_features');
+
+?>
